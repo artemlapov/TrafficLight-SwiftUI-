095a5e0770e1e965669d478ec03602357fc46b60
Artem LAPOV <artemlapov@me.com>
2022-12-09 12:35:41 +0100

    Initial Commit

---
+++ TrafficLight(SwiftUI)/TrafficLight_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_SwiftUI_App.swift
+//  TrafficLight(SwiftUI)
+//
+//  Created by Artem Lapov on 09.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
